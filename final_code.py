82ff2eef6f1801e089db58932a74790467675df7
poojashresthacode <143756553+poojashresthacode@users.noreply.github.com>
2023-10-18 05:10:12 +0545

    Rename actual periodic time to actual_p_t.

--- final_code.py
+++ final_code.py
@@ -12,12 +12,12 @@ class TestCalculatePeriodicTime(unittest.TestCase):
         expected_periodic_times = [1.4185, 2.006, 2.451, 2.828, 3.162]
 
         # Use the calculate_periodic_time() function to calculate the periodic times
-        actual_periodic_times = list(map(calculate_periodic_time, x))
+        actual_p_t = list(map(calculate_periodic_time, x))
 
         # Print and assert that the actual periodic times are close to the expected periodic times
         for i in range(len(x)):
-            print(f"For l = {x[i]}, Expected: {expected_periodic_times[i]}, Got: {actual_periodic_times[i]}")
-            assert math.isclose(actual_periodic_times[i], expected_periodic_times[i], rel_tol=1e-2)  # Increased tolerance
+            print(f"For l = {x[i]}, Expected: {expected_periodic_times[i]}, Got: {actual_p_t[i]}")
+            assert math.isclose(actual_p_t[i], expected_periodic_times[i], rel_tol=1e-2)  # Increased tolerance
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
