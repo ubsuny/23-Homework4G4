643054a87673a9a2f93e666b6ebeb43e8008075d
poojashresthacode <143756553+poojashresthacode@users.noreply.github.com>
2023-10-17 21:12:17 +0545

    Update periodic_time.py file

--- periodic_time.py
+++ periodic_time.py
@@ -1,20 +1,5 @@
-import unittest
 import math
-
-def calculate_periodic_time(l):
-    """Calculates the periodic time of a simple pendulum.
-
-    Args:
-      l: The length of the pendulum in meters.
-
-    Returns:
-      The periodic time of the pendulum in seconds.
-    """
-    # Acceleration due to gravity in meters per second squared.
-    g = 9.81
-    
-    # Correct formula for the period of a simple pendulum
-    return 2 * math.pi * math.sqrt(l / g)
+import module
 
 class TestCalculatePeriodicTime(unittest.TestCase):
 
